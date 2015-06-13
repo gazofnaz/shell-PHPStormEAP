ff469a91187772646b4f7cbd0b8dfda27d7e3ae4
Gareth Arnott <gareth.arnott@informa.com>
2015-06-13 14:29:59 +0100

    working fetch, download, and extract

--- ps.sh
+++ ps.sh
@@ -3,13 +3,18 @@
 # the latest eap version of phpstorm to
 # ~/Downloads.
 
+DIRECTORY=$HOME/Downloads/
+echo "\nFetching download link...\n"
 LINK=`wget -qO- 'https://confluence.jetbrains.com/display/PhpStorm/PhpStorm+Early+Access+Program' | grep -oP "(?<=<a href=\").*?(?=\.tar\.gz)" | awk '{print $0 ".tar.gz"}'`
 FILENAME=`basename "$LINK"`
-echo "$FILENAME"
-#echo -n "Downloading...   "
-#wget -P ~/Downloads/ --progress=dot "$LINK" 2>&1 | grep --line-buffered "%" | sed -u -e "s,\.,,g" | awk '{printf("\b\b\b\b%4s", $2)}'
-#echo -ne "\b\b\b\b"
-#echo "Done"
-#echo -n "Extracting...   "
-
+#@todo, check if file exists before downloading
+echo "Downloading...      "
+wget -P "$DIRECTORY" --progress=dot "$LINK" 2>&1 | grep --line-buffered "%" | sed -u -e "s,\.,,g" | awk '{printf("\b\b\b\b%4s", $2)}'
+echo "\nDone downloading\n"
+echo "Extracting...\n"
+#@todo check if folder exists before extracting
+#@todo report tar errors to user
+tar -xf "$DIRECTORY$FILENAME" -C "$DIRECTORY"
+echo "Done extracting\n"
+#@todo delete zip after extracting
 
