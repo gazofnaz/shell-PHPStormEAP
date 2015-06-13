0a76e4e1fd63cb0844d856423efa6fe42b9dc738
Gareth Arnott <gareth.arnott@informa.com>
2015-06-13 15:59:52 +0100

    add check for existing tar.gz

--- ps.sh
+++ ps.sh
@@ -7,7 +7,14 @@ DIRECTORY=$HOME/Downloads/
 echo "\nFetching download link...\n"
 LINK=`wget -qO- 'https://confluence.jetbrains.com/display/PhpStorm/PhpStorm+Early+Access+Program' | grep -oP "(?<=<a href=\").*?(?=\.tar\.gz)" | awk '{print $0 ".tar.gz"}'`
 FILENAME=`basename "$LINK"`
-#@todo, check if file exists before downloading
+
+# check if the file exists before downloading
+if [ -f $DIRECTORY$FILENAME ];
+then
+    echo You already have the latest  archive. Perhaps you forgot to extract it?
+    exit 0
+fi
+
 echo "Downloading...      "
 wget -P "$DIRECTORY" --progress=dot "$LINK" 2>&1 | grep --line-buffered "%" | sed -u -e "s,\.,,g" | awk '{printf("\b\b\b\b%4s", $2)}'
 echo "\nDone downloading\n"
